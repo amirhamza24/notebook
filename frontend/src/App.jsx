5a9406babb5ee52984b3489a6ee159660e1792b2
amirhamza24 <ridoykhan7897890@gmail.com>
2025-09-11 11:19:00 +0600

    project start

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl bg-red-500">Hello world</div>
+    </>
+  );
+}
+
+export default App;
