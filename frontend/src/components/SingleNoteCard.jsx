33f404a17c92b88b69b235350269f64bd217b1c4
amirhamza24 <ridoykhan7897890@gmail.com>
2025-09-18 16:21:13 +0600

    animated border design add

--- frontend/src/components/SingleNoteCard.jsx
+++ frontend/src/components/SingleNoteCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { FiEdit } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
+import "../styles/style.css";
 
 export default function SingleNoteCard({ note }) {
   console.log("single note card: ", note);
 
   return (
-    <div className="bg-white p-4 rounded-md border-[1px] border-gray-200 hover:shadow-[0_0_70px_0_rgba(0,0,0,0.1)] hover:bg-gray-50 transition-shadow duration-200 cursor-pointer">
+    // <div className="bg-white p-4 rounded-md border border-gray-200 hover:border-blue-500 hover:shadow-[0_0_70px_0_rgba(0,0,0,0.1)] hover:bg-gray-50 transition-all duration-300 ease-in-out cursor-pointer card">
+    <div className="cursor-pointer card hover:shadow-[0_0_70px_0_rgba(0,0,0,0.1)] ">
       <h2 className="text-xl font-bold">{note.title}</h2>
       <p>{note.description}</p>
 
