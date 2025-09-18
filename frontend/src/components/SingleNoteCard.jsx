3277b04568bd9b251a872588b5ebbcc8b20a4497
amirhamza24 <ridoykhan7897890@gmail.com>
2025-09-18 18:01:25 +0600

    truncate text title description

--- frontend/src/components/SingleNoteCard.jsx
+++ frontend/src/components/SingleNoteCard.jsx
@@ -6,17 +6,30 @@ import "../styles/style.css";
 export default function SingleNoteCard({ note }) {
   console.log("single note card: ", note);
 
+  const truncateText = (text, limit) => {
+    if (!text) return "";
+    return text.length > limit ? text.substring(0, limit) + "..." : text;
+  };
+
   return (
     // <div className="bg-white p-4 rounded-md border border-gray-200 hover:border-blue-500 hover:shadow-[0_0_70px_0_rgba(0,0,0,0.1)] hover:bg-gray-50 transition-all duration-300 ease-in-out cursor-pointer card">
-    <div className="cursor-pointer card hover:shadow-[0_0_70px_0_rgba(0,0,0,0.1)] ">
-      <h2 className="text-xl font-bold">{note.title}</h2>
-      <p>{note.description}</p>
+    <div className="card hover:shadow-[0_0_70px_0_rgba(0,0,0,0.1)] flex flex-col">
+      <div>
+        <h2 className="text-xl font-bold">
+          {/* {note.title} */}
+          {truncateText(note.title, 15)}
+        </h2>
+        <p>
+          {/* {note.description} */}
+          {truncateText(note.description, 30)}
+        </p>
+      </div>
 
-      <div className="flex justify-end mt-2">
-        <button className="text-blue-500 mr-2">
+      <div className="flex justify-end mt-2 text-lg">
+        <button className="text-blue-500 mr-2 cursor-pointer">
           <FiEdit />
         </button>
-        <button className="text-red-500">
+        <button className="text-red-500 cursor-pointer">
           <FaTrash />
         </button>
       </div>
